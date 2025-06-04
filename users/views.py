b4405d3b44f02c49ffbbfb3da980d0461d5cbe6b
Ulug'bek G'ozixonov <ulugbekgozixonov@gmail.com>
2025-06-04 16:32:15 +0500

    regsiter endded begin status

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
-# Create your views here.
+class RegisterAPIView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            # Save the user data to the database
+            serializer.save()
+            return Response({"message": "User registered successfully."}, status=201)
+        return Response(serializer.errors, status=400)
