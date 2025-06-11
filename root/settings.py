d25f2f657684d018b9c4deb15104b91b72307288
Ulug'bek G'ozixonov <ulugbekgozixonov@gmail.com>
2025-06-11 15:42:08 +0500

    register qismi o'zgartirildi statuslar kerak mas shunchaki malumot kiritadi va tasdiqlash kodi boradi va token beriladi

--- root/settings.py
+++ root/settings.py
@@ -75,6 +75,12 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
